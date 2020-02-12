2e441d281f5a94fd9819a1964b3203f3f74ceb7a
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-12 04:55:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,10 @@ class Student
   def self.drop_table 
     sql =  <<-SQL 
     DROP TABLE students (
-    )
+    id INTEGER PRIMARY KEY, 
+        name TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
  
 end
